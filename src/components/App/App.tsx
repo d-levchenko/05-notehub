6241c0ab38f2b77b049b718b4889ccff8efbde14
d-levchenko <dl.qubb@gmail.com>
2026-04-12 22:10:34 +0300

    initial project commit; prepared to work

---
+++ src/components/App/App.tsx
@@ -0,0 +1,15 @@
+import css from './App.module.css';
+
+const App = () => {
+  return (
+    <div className={css.app}>
+      <header className={css.toolbar}>
+        {/* Компонент SearchBox */}
+        {/* Пагінація */}
+        {/* Кнопка створення нотатки */}
+      </header>
+    </div>
+  );
+};
+
+export default App;
