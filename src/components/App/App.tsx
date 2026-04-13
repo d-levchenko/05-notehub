dd3dcdd6d2bfaf9d7bd2d5bf7ff8d5d89c532afd
d-levchenko <dl.qubb@gmail.com>
2026-04-13 22:19:33 +0300

    form done, modal done, button for modal will be updated later

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -5,12 +5,16 @@ import { useDebouncedCallback } from 'use-debounce';
 
 import NoteList from '../NoteList/NoteList';
 import SearchBox from '../SearchBox/SearchBox';
+import Pagination from '../Pagination/Pagination';
+import NoteForm from '../NoteForm/NoteForm';
 
 import fetchNotes from '../../services/noteService';
+import Modal from '../Modal/Modal';
 
 const App = () => {
   const [page, setPage] = useState(1);
   const [search, setSearch] = useState('');
+  const [modalOpen, setModalOpen] = useState(false);
   const PER_PAGE = 12;
 
   const debouncedSearch = useDebouncedCallback((value: string) => {
@@ -25,16 +29,35 @@ const App = () => {
     placeholderData: keepPreviousData,
   });
 
+  const totalPages = data?.totalPages ?? 0;
+
   return (
     <div className={css.app}>
       <header className={css.toolbar}>
         <SearchBox onChange={debouncedSearch} />
-        {/* Пагінація */}
-        <button className={css.button}>Create note +</button>
+        {data?.notes && data.notes.length > 0 && (
+          <>
+            {totalPages > 1 && (
+              <Pagination
+                totalPages={totalPages}
+                currentPage={page}
+                onPageChange={setPage}
+              />
+            )}
+          </>
+        )}
+        <button className={css.button} onClick={() => setModalOpen(true)}>
+          Create note +
+        </button>
       </header>
       {isLoading && <div>Loading...</div>}
       {isError && <div>There is an error to load notes.</div>}
       {data && data.notes.length > 0 && <NoteList notes={data.notes} />}
+      {modalOpen && (
+        <Modal onClose={() => setModalOpen(false)}>
+          <NoteForm onCancel={() => setModalOpen(false)} />
+        </Modal>
+      )}
     </div>
   );
 };
--- src/components/Pagination/Pagination.tsx
+++ src/components/Pagination/Pagination.tsx
@@ -2,6 +2,8 @@ import ReactPaginateModule from 'react-paginate';
 import type { ReactPaginateProps } from 'react-paginate';
 import type { ComponentType } from 'react';
 
+import css from './Pagination.module.css';
+
 type ModuleWithDefault<T> = { default: T };
 
 const ReactPaginate = (
@@ -10,4 +12,30 @@ const ReactPaginate = (
   >
 ).default;
 
-export default ReactPaginate;
+interface PaginationProps {
+  totalPages: number;
+  currentPage: number;
+  onPageChange: (nextPage: number) => void;
+}
+
+const Pagination = ({
+  totalPages,
+  currentPage,
+  onPageChange,
+}: PaginationProps) => {
+  return (
+    <ReactPaginate
+      pageCount={totalPages}
+      pageRangeDisplayed={5}
+      marginPagesDisplayed={1}
+      onPageChange={({ selected }) => onPageChange(selected + 1)}
+      forcePage={currentPage - 1}
+      containerClassName={css.pagination}
+      activeClassName={css.active}
+      nextLabel="→"
+      previousLabel="←"
+    />
+  );
+};
+
+export default Pagination;
